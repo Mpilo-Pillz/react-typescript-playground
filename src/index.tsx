41c1d2938ce704615fe40a2c0c6dd997fa391007
Fikasentani <mpilo.dlamini@yahoo.com>
2021-04-15 06:52:41 +0200

    this commit reminds me that property and constructor prop initialization are different in TS and JS

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,21 @@ import ReactDOM from "react-dom";
 interface AppProps {
   color: string;
 }
+// interface AppState {
+//     counter: number
+// }
 class App extends React.Component<AppProps> {
-  state = { counter: 0 };
+  constructor(props: AppProps) {
+    super(props);
+
+    this.state = { counter: 0 };
+  }
+  /*
+  //state = { counter: 0 }; defineing properites here and in the constructor is not the smae
+  in ts as it in in JS
+  so you have to chose between initializing the property way of initialising a state object
+  or chose the constructor way of initializing a state <object data. In JS we dont have to worry abou this" type="" className=""></object>
+*/
 
   onIncrement = (): void => {
     console.log("increase" + this.state.counter);
