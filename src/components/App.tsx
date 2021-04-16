173363cdcc0cd1fc88166acf2e1728acf7c72607
Fikasentani <mpilo.dlamini@yahoo.com>
2021-04-16 07:23:17 +0200

    REFERENCE POINT - CONNECTING STORE TO APP COMPONENT

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,30 @@
 import React from "react";
+import { connect } from "react-redux";
+import { Todo, fetchTodos } from "../actions";
+import { StoreState } from "../reducers";
 
-export class App extends React.Component {
+interface AppProps {
+  todos: Todo[];
+  fetchTodos(): any;
+}
+
+class _App extends React.Component<AppProps> {
+  componentDidMount() {
+    this.props.fetchTodos();
+  }
   render() {
+    console.log(this.props.todos);
+
     return <div>Mub</div>;
   }
 }
+
+const mapStateToProps = ({ todos }: StoreState): { todos: Todo[] } => {
+  return { todos };
+};
+
+export const App = connect(mapStateToProps, { fetchTodos })(_App);
+
+// const mapStateToProps = (state: StoreState): {todos: Todo[]} => {
+//   return {todos: state.todos };
+// }
--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,5 +1,11 @@
 import { combineReducers } from "redux";
+import { Todo } from "../actions";
+import { todosReducer } from "./todos";
 
-export const reducers = combineReducers({
-  counter: () => 1,
+export interface StoreState {
+  todos: Todo[];
+} //describes how our entire application state should look
+
+export const reducers = combineReducers<StoreState>({
+  todos: todosReducer,
 });
