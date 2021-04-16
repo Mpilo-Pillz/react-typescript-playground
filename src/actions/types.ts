371bd1e37b151655b2d074aca9a040d3f0b0c786
Fikasentani <mpilo.dlamini@yahoo.com>
2021-04-16 07:05:15 +0200

    setting up todo reducer

---
+++ src/actions/types.ts
@@ -0,0 +1,12 @@
+export enum ActionTypes {
+  fetchTodos,
+}
+
+// export enum ActionTypes {
+//     fetchTodos = "FETCH_TODOS"
+// }
+/*
+ * THE ABOVE IS A SHORT CUT WE DONT HAVE TO SPECIFY "FETCH_TODOS"
+ * ALL IT NEEDS TO DO IS JUST BE UNIQUE
+ * SO IF WE DONT SPECIFY THEN THE DEFUALT OS SET TO 0 THEN 1 THEN 2 ETC AND THAT IS UNIQUE
+ */
